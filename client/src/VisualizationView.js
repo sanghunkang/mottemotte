66bd7662882093154f5c36c52ae54fe9d404db2a
sanghunkang <sanghunkang.dev@gmail.com>
2019-07-17 01:10:50 +0900

    implementing vertical scroll

--- client/src/VisualizationView.js
+++ client/src/VisualizationView.js
@@ -1,5 +1,5 @@
 import * as d3 from "d3";
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 
 import './VisualizationView.css'
 
@@ -9,17 +9,6 @@ const HIGHLIGHT_OFF = 'green';
 const languagePack = {
   day: '일'
 }
-// var toggleColor = (()=>{
-//   var currentColor = "green";
-//   return function() {
-//     console.log(this);
-//     let y = d3.select(this).attr("y");
-//     currentColor = currentColor === "green" ? "magenta" : "green";
-//     d3.select(this)
-//       .attr('fill', currentColor)
-//       .attr("y", 100);
-//   }
-// })();
 
 // Event Handlers
 var onClickItem = function() {
@@ -34,7 +23,7 @@ var onClickItem = function() {
   }
 }();
 
-var onMouseEnterItem = function(){
+var onMouseEnterItem = function() {
   return function() {
     if (this.classList.contains('clicked') === false) {
       d3.select(this)
@@ -45,7 +34,6 @@ var onMouseEnterItem = function(){
 
 var onMouseLeaveItem = function(){
   return function() {
-    // console.log(this.classList.contains('clicked'))
     if (this.classList.contains('clicked') === false) {
       d3.select(this)
         .attr('fill', HIGHLIGHT_OFF)
@@ -53,9 +41,51 @@ var onMouseLeaveItem = function(){
   }
 }();
 
+// d3 rendering functions
+// Closure contexts with dependencies on browsers
+let h = 500;
+
+
+var drawItemBoxes = function() {
+  return function(svg, data, zoom=1) {
+    svg.selectAll(".Box")
+      .remove();
+
+    console.log(65 * zoom);
+    svg.selectAll(".Box")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "Box")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => h - 10 * d)
+      .attr("z", (d, i) => 1)
+      .attr("width", (65 * zoom))
+      .attr("height", (d, i) => d * 10)
+      .attr('fill', "green");
+  }
+}();
+
+var addDateIndeces = function() {
+  return function(svg, data, indexStart) {  
+    svg.selectAll(".DateIndex")
+      .remove();
+
+    svg.selectAll(".DateIndex")
+      .data(data)
+      .enter()
+      .append("text")
+      .attr("class", 'DateIndex')
+      .text((d) => (d + indexStart + 1) + languagePack.day)
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => h)
+      .attr("z", (d, i) => 999);
+  }
+}();
+
 // Functional React Components
 function VisualizationView() {
-  useEffect(() => drawChart());
+  useEffect(() => drawChart()); 
   
   return(
     <div className="VisualizationView">
@@ -67,28 +97,26 @@ function VisualizationView() {
 
 function drawChart() {
   let data = [12, 5, 6, 6, 9, 10, 2, 3, 6, 3, 3, 3, 6, 3, 4, 3, 2, 12, 11, 32, 12, 32, 15];
-  let w = 700;
-  let h = 500;
 
   
   let indexStart = 0;
   let range = 14;
   let stride = 15;
   let sumDeltaX = 0;
-  
+  let sumDeltaY = 0;
+  let zoom = 0;
   
   const svg = d3.select(".BarChart");
-  drawItemBoxes(svg, data.slice(indexStart,indexStart+range), w, h);
-  addDateIndeces(svg, [...Array(range).keys()], w, h, indexStart);
+  drawItemBoxes(svg, data.slice(indexStart,indexStart+range));
+  addDateIndeces(svg, [...Array(range).keys()], indexStart);
   addEventHandlers(svg);                  
   
-  // svg.on('mousewheel', onMouseMoveBoard(svg, data));
   svg.on('mousewheel', ()=> {
+    // Event handling for horizontal scroll
     if (sumDeltaX < -stride || stride < sumDeltaX ) {
-      console.log(data.slice(indexStart,indexStart+range));
       indexStart += sumDeltaX < 0? -1: 1;
-      drawItemBoxes(svg, data.slice(indexStart,indexStart+range), w, h);
-      addDateIndeces(svg, [...Array(range).keys()], w, h, indexStart);
+      drawItemBoxes(svg, data.slice(indexStart,indexStart+range));
+      addDateIndeces(svg, [...Array(range).keys()], indexStart);
       addEventHandlers(svg);
       sumDeltaX = 0;
     } 
@@ -98,8 +126,19 @@ function drawChart() {
     } else if (indexStart !== data.length - stride &&  0 < d3.event.deltaX) { // Upper bound
       sumDeltaX += d3.event.deltaX;
     }
+
+    // Event handling for vertical scroll
+    if (sumDeltaY < -200 || 200 < sumDeltaY ) {
+      console.log(sumDeltaY);
+      zoom += sumDeltaY < 0? 0.1: 0.1;
+      drawItemBoxes(svg, data.slice(indexStart,indexStart+range), zoom);
+      addDateIndeces(svg, [...Array(range).keys()], indexStart);
+      addEventHandlers(svg);
+      sumDeltaY = 0;
+    }
+    sumDeltaY += d3.event.deltaY;
+
   });
-  
   
 
 //   svg.selectAll("text")
@@ -109,50 +148,36 @@ function drawChart() {
 //     .text((d) => d)
 //     .attr("x", (d, i) => i * 70)
 //     .attr("y", (d, i) => h - (10 * d) - 3);
-
-
 }
  
 
 
-function drawItemBoxes(svg, data, w, h) {
-  svg.selectAll(".Box")
-    .remove();
 
-  svg.selectAll(".Box")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("class", "Box")
-    .attr("x", (d, i) => i * 70)
-    .attr("y", (d, i) => h - 10 * d)
-    .attr("z", (d, i) => 1)
-    .attr("width", 65)
-    .attr("height", (d, i) => d * 10)
-    .attr('fill', "green");
-}
+// function drawItemBoxes(svg, data, w, h) {
+//   svg.selectAll(".Box")
+//     .remove();
+
+//   svg.selectAll(".Box")
+//     .data(data)
+//     .enter()
+//     .append("rect")
+//     .attr("class", "Box")
+//     .attr("x", (d, i) => i * 70)
+//     .attr("y", (d, i) => h - 10 * d)
+//     .attr("z", (d, i) => 1)
+//     .attr("width", 65)
+//     .attr("height", (d, i) => d * 10)
+//     .attr('fill', "green");
+// }
 
 function addEventHandlers(svg) {
-  svg.selectAll("rect")
-    .on("click", onClickItem)
+  svg.selectAll('.Box')
+    .on('click', onClickItem)
     .on('mouseenter', onMouseEnterItem)
     .on('mouseleave', onMouseLeaveItem)
     
 }
 
-function addDateIndeces(svg, data, w, h, indexStart) {  
-  svg.selectAll(".DateIndex")
-    .remove();
 
-  svg.selectAll(".DateIndex")
-    .data(data)
-    .enter()
-    .append("text")
-    .attr("class", 'DateIndex')
-    .text((d) => (d + indexStart + 1) + languagePack.day)
-    .attr("x", (d, i) => i * 70)
-    .attr("y", (d, i) => h)
-    .attr("z", (d, i) => 999);
-}
 
 export default VisualizationView;
